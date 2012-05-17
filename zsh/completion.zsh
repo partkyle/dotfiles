aae980c4ddb7d25cb093aa5b7fa7613e13817a99
Kyle Partridge <partkyle@gmail.com>
2012-05-17 00:29:20 -0700

    added my completion over

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,12 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# add menu selecting with the cursor
+zstyle ':completion:*:*:*:*:*' menu select
+
+# pasting with tabs doesn't perform completion
+zstyle ':completion:*' insert-tab pending
+
+# colors
+zstyle ':completion:*' list-colors ''
