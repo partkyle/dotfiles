844b59125d2456d5ec87c0385c9c556a958d8d3e
Kyle Partridge <partkyle@gmail.com>
2012-05-17 00:15:50 -0700

    added new git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,7 @@ then
 fi
 
 # The rest of my fun git aliases
+alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -16,4 +17,5 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gst='git status'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
