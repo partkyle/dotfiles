c3355122d2145c4f0fe1cb2be518004d5fa46ba4
Kyle Partridge <partkyle@gmail.com>
2012-05-17 00:16:30 -0700

    noglob fixes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
 alias reload!='. ~/.zshrc'
+
+# noglob fixes
+alias curl='noglob curl'
+alias echo='noglob echo'
+alias find='noglob find'
+alias grep='noglob grep'
+alias egrep='noglob egrep'
+alias fgrep='noglob fgrep'
