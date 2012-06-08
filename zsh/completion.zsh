f228f45e4da2a81122cda12076ef76335b5d78e3
Kyle Partridge <kyle.partridge@sendgrid.com>
2012-06-08 09:30:54 -0700

    better zsh completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,7 @@
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+
 # matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
