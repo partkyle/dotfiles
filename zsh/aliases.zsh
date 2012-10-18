47a731e54df43320a2706ad16508f934ecbc5541
Kyle Partridge <kyle.partridge@sendgrid.com>
2012-10-18 11:34:57 -0700

    added over more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,9 @@
+#
+# util aliases
+#
+
 alias reload!='. ~/.zshrc && echo "Shoot outside the screen!"'
 alias :q='exit'
-
-# lgst, since I type it all the time
-alias lgst='git status; ls'
 
 # noglob fixes
 alias  curl='nocorrect curl'
@@ -17,3 +18,26 @@ alias   ack='nocorrect ack'
 alias  grep='nocorrect grep --color=auto'
 alias egrep='nocorrect egrep --color=auto'
 alias fgrep='nocorrect fgrep --color=auto'
+
+#
+# git aliases
+
+alias g='git'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gst='git status'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+# lgst, since I type it all the time
+alias lgst='git status; ls'
+
+#
+# ruby aliases
+#
+
+# pow restart
+alias rst='touch tmp/restart.txt'
+
+# bundler
+alias be='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
